897b51c8dcc123c86cac796ae2519e4ff3e0676d
angellmagri <angell_magri@estudante.sesisenai.org.br>
2024-04-01 20:44:16 -0300

    ex.02

--- semana09/app.js
+++ semana09/app.js
@@ -5,12 +5,23 @@
 
 const express = require("express")
 const app = express()
+app.use(express.json())
 
-app.listen(3000, function(){
+app.listen(3033, function(){
     console.log("Servidor Rodando!")
- }) // esse '3000' é uma 'porta'
+ }) // Não funcionou com a porta 3000 - poderia já estar ocupada.
 
 
 // EXERCÍCIO 2: Adicione rotas ao novo projeto:
 //Uma rota GET que responda à URL '/sobre' com uma mensagem sobre o seu aplicativo.
 //Uma rota GET que responda à URL '/contato' com uma mensagem de contato.
+
+app.get("/sobre",function(req, res){
+    res.send("Esse é um aplicativo criado para responder aos exercícios da semana 9!")
+})
+ // acessar: http://localhost:3033/sobre
+
+ app.get("/contato",function(req, res){
+    res.send("Contate-nos pelo e-mail: angell_magri@estudante.sesisenai.org.br")
+})
+ // acessar: http://localhost:3033/contato
